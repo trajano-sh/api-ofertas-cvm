aa59491b01b0d82623faab9f62c768a5898779b5
TrajanoXT <viniciustrajano46@gmail.com>
2026-07-31 18:55:30 -0300

    Primeira entidade register criada

---
+++ src/main/java/br/com/cvm/api_ofertas/database/entities/Register.java
@@ -0,0 +1,55 @@
+package br.com.cvm.api_ofertas.database.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Table(name = "registers")
+public class Register {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String idRequerimento;
+
+    @Column(unique = true, nullable = false)
+    private String numeroProtocolo;
+
+    @Column(unique = true, nullable = false)
+    private String numeroProcesso;
+
+    private String nomeValorMobiliario;
+
+    private String tipoDeOferta;
+
+    private String statusDaOferta;
+
+    private String nomeEmissor;
+
+    private String cnpjEmissor;
+
+    private String nomeCoordenadorLider;
+
+    private String cnpjCoordenadorLider;
+
+    private String valorEmReais;
+
+    private LocalDate data;
+
+    private String nomeTipoRequerimento;
+
+    private boolean vasoComunicante;
+
+    private boolean possuiBook;
+
+    private boolean registroAutomatico;
+}
