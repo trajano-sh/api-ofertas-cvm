0299671609b9808e6b53379fc34edb6b11d152d3
TrajanoXT <viniciustrajano46@gmail.com>
2026-07-31 16:17:05 -0300

    Add estrutura de pastas

---
+++ src/main/java/br/com/cvm/api_ofertas/ApiOfertasApplication.java
@@ -0,0 +1,13 @@
+package br.com.cvm.api_ofertas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiOfertasApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(ApiOfertasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/cvm/api_ofertas/ApiOfertasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cvm.api_ofertas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiOfertasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
